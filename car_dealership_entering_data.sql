3a051c0ea01aa5bc93c26bfac9629dfabcdf1305
crwill83 <99084807+crwill83@users.noreply.github.com>
2022-03-07 11:10:49 -0700

    added functions and procedures

--- car_dealership_entering_data.sql
+++ car_dealership_entering_data.sql
@@ -195,6 +195,80 @@ insert into service_ticket_mechanic(
 values (3 , 1);
 
 
+-- let's create a stored procedure for a 5% discount
+CREATE OR REPLACE PROCEDURE holiday_discount_invoice(
+	customer INTEGER,
+	discount DECIMAL
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	-- Update invoice amount for 5% discount
+	UPDATE invoice
+	SET invoice_amount = invoice_amount * discount 
+	WHERE customer_id = customer;
+	
+	-- Commit the above statement inside of a transaction
+	COMMIT;
+	
+END;
+$$
+
+-- now we'll call that procedure
+CALL holiday_discount_invoice(1 , 0.95); 
+
+-- now we'll add a procedure that gives a free oil change with purchase of 4 snow tires and apply it to Julie
+CREATE OR REPLACE PROCEDURE free_oil_with_tires(
+	customer INTEGER,
+	discount DECIMAL
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	-- Update service_ticket to apply $40 discount
+	UPDATE service_ticket 
+	SET service_amount = service_amount - discount
+	WHERE parts = 'snow tires';
+	
+	-- Commit the above statement inside of a transaction
+	COMMIT;
+	
+END;
+$$
+
+-- and we'll call this one too
+CALL free_oil_with_tires (5 , 40)
+
+
+-- stored function to add a customer
+CREATE OR REPLACE FUNCTION add_customer(_c_id INTEGER, _f_name VARCHAR, _l_name VARCHAR, _age_ INTEGER , _address_ VARCHAR , _email VARCHAR)
+RETURNS void 
+AS $MAIN$
+BEGIN
+	INSERT INTO "Customer"(customer_id,first_name,last_name,age,address,email)
+	VALUES(_c_id , _f_name, _l_name, _age_, _address_, _email);
+END;
+$MAIN$
+LANGUAGE plpgsql;
+
+-- now we'll add a customer
+SELECT add_customer(6,'Jack','Sparrow',47,'143 Black Pearl','captain@blackpearl.com')
+
+
+-- one more stored function to add a car since stock is getting low
+CREATE OR REPLACE FUNCTION add_car(_c_id INTEGER, _make VARCHAR, _model VARCHAR, _c_year INTEGER , _license VARCHAR)
+RETURNS void 
+AS $MAIN$
+BEGIN
+	INSERT INTO car(car_id,car_make,car_model,car_year,license_plate)
+	VALUES(_c_id , _make, _model, _c_year, _license);
+END;
+$MAIN$
+LANGUAGE plpgsql;
+
+-- now we call the function and add a car
+SELECT add_car(6,'ford','F650',2018,'LA6789')
+
 
 --
 select * from "Customer"; 
